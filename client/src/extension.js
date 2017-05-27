84a528e74a1979dfc86bd0c2f1ee1ca69f7935ce
Antonino Perricone <antonino.perricone@yahoo.it>
2017-05-27 10:29:45 +0200

    validation refactoring support for diagnostics in other files

--- client/src/extension.js
+++ client/src/extension.js
@@ -1,7 +1,7 @@
 var vscode = require('vscode');
 var client = require('vscode-languageclient');
 var path = require('path');
-var cp = require("child_process");
+var validation = require('./validation.js');
 
 var diagnosticCollection;
 
@@ -12,76 +12,16 @@ function activate(context) {
 			decreaseIndentPattern: /^\s*(end[a-z]*|next|else|elseif|next)\b/i
 		}
 	});
-    diagnosticCollection = vscode.languages.createDiagnosticCollection('harbour');
-	vscode.workspace.onDidOpenTextDocument(validate,undefined, context.subscriptions);
-	vscode.workspace.onDidSaveTextDocument(validate,undefined, context.subscriptions);
-	vscode.workspace.onDidCloseTextDocument(removeValidation,undefined, context.subscriptions);
-	context.subscriptions.push(diagnosticCollection);
-	
-	/*	
-	let serverOptions = {
-		command:  context.asAbsolutePath(path.join( 'bin', 'server'))
-		//,env:{LD_LIBRARY_PATH: ":/home/perry/harbour-src/lib/linux/gcc/"}
-	}
-	let clientOptions = {
-		documentSelector: ['harbour'],
-		sincronize: {
-			configurationSection: 'harbour'
-		},
-		fileEvents: vscode.workspace.createFileSystemWatcher('** /.clientrc')
-	}
-	//let disposable = new client.LanguageClient('Harbour language server', 'Harbour language server', 
-	//		serverOptions, clientOptions).start();
-	
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
-	//context.subscriptions.push(disposable);	*/
+	validation.activate(context);
+	vscode.workspace.onDidOpenTextDocument(validation.validate,undefined, context.subscriptions);
+	vscode.workspace.onDidSaveTextDocument(validation.validate,undefined, context.subscriptions);
+	vscode.workspace.onDidCloseTextDocument(validation.removeValidation,undefined, context.subscriptions);
 }
 
 function deactivate() {
-	 diagnosticCollection.dispose();
+	 validation.deactivate();
 }
 
 exports.activate = activate;
 exports.deactivate = deactivate;
 
-// validation
-var valRegEx = /([^\(]*)\((\d+)\)\s+(Warning|Error)\s+(.*)/
-function validate(textDocument)
-{
-	if(textDocument.languageId !== 'harbour' )
-		return;
-	var section = vscode.workspace.getConfiguration('harbour');
-	if(!section.validating)
-		return;
-	var args = ["-s", "-q0", "-w"+section.warningLevel, textDocument.fileName ];
-	for (var i = 0; i < section.extraIncludePaths.length; i++) {
-		var path = section.extraIncludePaths[i];
-		args.push("-i"+path);
-	}
-	var process = cp.spawn(section.compilerExecutable,args, { cwd: vscode.workspace.path });
-	var diagnostics = [];
-	process.stderr.on('data', data => 
-	{
-		//console.error(data.toString())
-		var r = valRegEx.exec(data.toString().trim());
-		if(r)
-		{
-			var line = parseInt(r[2])-1;
-			diagnostics.push(new vscode.Diagnostic(new vscode.Range(line,0,line,Number.MAX_VALUE),
-				r[4], r[3]=="Warning"? 1 : 0))
-		}
-	});
-	process.stdout.on('data', data => 
-		console.log(data.toString())
-	);
-	process.on("exit",function(code)
-	{
-		diagnosticCollection.set(textDocument.uri, diagnostics);
-	});
-}
-
-function removeValidation(textDocument)
-{
-	diagnosticCollection.delete(textDocument.uri);
-}
---
+++ client/src/validation.js
@@ -0,0 +1,71 @@
+var vscode = require('vscode');
+var cp = require("child_process");
+
+var diagnosticCollection;
+
+function activate(context) 
+{
+    diagnosticCollection = vscode.languages.createDiagnosticCollection('harbour');
+	context.subscriptions.push(diagnosticCollection);
+}
+
+function deactivate()
+{
+	 diagnosticCollection.dispose();
+}
+
+var valRegEx = /([^\(]*)\((\d+)\)\s+(Warning|Error)\s+(.*)/
+function validate(textDocument)
+{
+	if(textDocument.languageId !== 'harbour' )
+		return;
+	var section = vscode.workspace.getConfiguration('harbour');
+	if(!section.validating)
+		return;
+	var args = ["-s", "-q0", "-m", "-n0", "-w"+section.warningLevel, textDocument.fileName ];
+	for (var i = 0; i < section.extraIncludePaths.length; i++) {
+		var path = section.extraIncludePaths[i];
+		args.push("-i"+path);
+	}
+	var process = cp.spawn(section.compilerExecutable,args, { cwd: vscode.workspace.path });
+	var diagnostics = {};
+	diagnostics[textDocument.fileName] = [];
+	process.stderr.on('data', data => 
+	{
+		//console.error(data.toString())
+		var r = valRegEx.exec(data.toString().trim());
+		if(r)
+		{
+			var line = parseInt(r[2])-1;
+			if(!r[1] in diagnostics)
+			{
+				diagnostics[r[1]] = [];
+			}
+			diagnostics[r[1]].push(new vscode.Diagnostic(new vscode.Range(line,0,line,Number.MAX_VALUE),
+				r[4], r[3]=="Warning"? 1 : 0))
+		}
+	});
+	process.stdout.on('data', data => 
+		console.log(data.toString())
+	);
+	process.on("exit",function(code)
+	{
+		for (var file in diagnostics) {
+			if (diagnostics.hasOwnProperty(file)) {
+				var infos = diagnostics[file];
+				diagnosticCollection.set(vscode.Uri.file(file), infos);		
+			}
+		}
+		//diagnosticCollection.set(textDocument.uri, diagnostics[textDocument.fileName]);
+	});
+}
+
+function removeValidation(textDocument)
+{
+	diagnosticCollection.delete(textDocument.uri);
+}
+
+exports.activate = activate;
+exports.deactivate = deactivate;
+exports.validate = validate;
+exports.removeValidation = removeValidation;
